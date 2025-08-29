491f466e2364660c375f54de45448006647f0dfb
Yuliana Gusakova <yulianagusakova@yandex.ru>
2025-08-29 22:31:07 +0300

    1. Собран Maven-проект: подключены Jacoco, Mockito и JUnit в pom.xml 2. В класс Lion добавлены инъекции зависимостей 3. Создан тестовый класс FelineTest

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import com.example.Feline;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class FelineTest {
+    @Test
+    public void eatMeatTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> foodList = feline.eatMeat();
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), foodList);
+    }
+
+    @Test
+    public void getFamilyTest() {
+        Feline feline = new Feline();
+        assertEquals("Кошачьи", feline.getFamily());
+    }
+
+    @Test
+    public void getKittensNoArgumentTest() {
+        Feline feline = new Feline();
+        assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void getKittensWithArgumentTest() {
+        Feline feline = new Feline();
+        assertEquals(4, feline.getKittens(4));
+    }
 }
